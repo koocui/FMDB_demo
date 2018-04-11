977588c034fd67ac18b7c7675eb4f4e24378b340
小崔 <xiaocui@xiaocuideMacBook-Pro.local>
2018-04-11 10:07:57 +0800

    创建demo

---
+++ FMDBDEMO/FMDBDEMO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDBDEMO
+//
+//  Created by 小崔 on 2018/4/10.
+//  Copyright © 2018年 CJW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDBDEMO/ThirdKu/NavView.m
@@ -0,0 +1,21 @@
+//
+//  NavView.m
+//  FMDBDEMO
+//
+//  Created by 小崔 on 2018/4/10.
+//  Copyright © 2018年 CJW. All rights reserved.
+//
+
+#import "NavView.h"
+
+@implementation NavView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
